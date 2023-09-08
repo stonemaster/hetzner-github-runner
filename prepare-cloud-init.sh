ee7ffcdb38b1617fb5a3eab904b7cb15c175be17
André Stein <stone@steinsoft.net>
2023-09-08 16:36:02 +0200

    Fix path issue.

--- prepare-cloud-init.sh
+++ prepare-cloud-init.sh
@@ -16,7 +16,7 @@ export PRE_JOB_SCRIPT=$(echo 'echo Hetzner runner has been started.' | base64)
 export POST_JOB_SCRIPT=$(echo 'killall Runner.Listener' | base64)
 # Configure server delete script to be called when runner is stopped. This stops
 # and removed the Hetzner cloud instance.
-export SHUTDOWN_RUNNER_SCRIPT=$(sed "s/hetzner_key=/hetzner_key=${HETZNER_API_KEY}/" delete-me.sh | base64)
+export SHUTDOWN_RUNNER_SCRIPT=$(sed "s/hetzner_key=/hetzner_key=${HETZNER_API_KEY}/" $(dirname $0)/delete-me.sh | base64)
 
 template_file=$(dirname $0)/cloud-init.yml.tmpl
 
