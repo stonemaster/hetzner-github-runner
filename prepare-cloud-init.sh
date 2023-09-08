21e7de2b573a63811de36b4f39b9837e5b050ac4
André Stein <stone@steinsoft.net>
2023-09-08 16:51:48 +0200

    Disable Base64 wrap.

--- prepare-cloud-init.sh
+++ prepare-cloud-init.sh
@@ -11,12 +11,12 @@ export GITHUB_REPO_URL=${1}
 export RUNNER_TOKEN=${2}
 export HETZNER_API_KEY=${3}
 
-export PRE_JOB_SCRIPT=$(echo 'echo Hetzner runner has been started.' | base64)
+export PRE_JOB_SCRIPT=$(echo 'echo Hetzner runner has been started.' | base64 -w0)
 # GitHub Actions internal binary name, is killed after job execution
-export POST_JOB_SCRIPT=$(cat $(dirname $0)/post-job-script.sh | base64)
+export POST_JOB_SCRIPT=$(cat $(dirname $0)/post-job-script.sh | base64 -w0)
 # Configure server delete script to be called when runner is stopped. This stops
 # and removed the Hetzner cloud instance.
-export SHUTDOWN_RUNNER_SCRIPT=$(sed "s/hetzner_key=/hetzner_key=${HETZNER_API_KEY}/" $(dirname $0)/delete-me.sh | base64)
+export SHUTDOWN_RUNNER_SCRIPT=$(sed "s/hetzner_key=/hetzner_key=${HETZNER_API_KEY}/" $(dirname $0)/delete-me.sh | base64 -w0)
 
 template_file=$(dirname $0)/cloud-init.yml.tmpl
 
