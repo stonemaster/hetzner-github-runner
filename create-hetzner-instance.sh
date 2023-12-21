e34185214d3ad08ebde722a8df94a51df2d15b85
André Stein <stone@steinsoft.net>
2023-12-21 09:48:40 +0100

    Add support for adding an SSH key ID to the deployed server.

--- create-hetzner-instance.sh
+++ create-hetzner-instance.sh
@@ -2,8 +2,8 @@
 
 set -eu -o pipefail
 
-if [[ $# != 5 ]]; then
-  echo "usage: $(basename $0) HETZNER_API_KEY INSTANCE_TYPE LOCATION_TYPE NAME_PREFIX CLOUD_INIT_YML_FILE"
+if [[ $# < 5 ]]; then
+  echo "usage: $(basename $0) HETZNER_API_KEY INSTANCE_TYPE LOCATION_TYPE NAME_PREFIX CLOUD_INIT_YML_FILE [SSH_KEY_ID]"
   exit 1
 fi
 
@@ -12,6 +12,7 @@ instance_type=${2}
 location=${3}
 name_prefix=${4}
 cloud_init_yml_file=${5}
+ssh_key_id=${6:-}
 
 template_file=$(dirname $0)/hetzner-server-create-template.jq
 
@@ -20,6 +21,10 @@ json=$(jq -n --arg location ${location} \
   --arg name "$name_prefix-$RANDOM" \
   --arg cloud_init_yml "$(cat $cloud_init_yml_file)" \
   -f ${template_file})
+
+if [ "$ssh_key_id" != "" ]; then
+  json=$(echo $json | jq ".ssh_keys += [$ssh_key_id]")
+fi
 
 curl \
   -X POST \
